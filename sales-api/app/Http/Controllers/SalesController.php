2ea0861fad090ce0258d2f7b1c87bb188270b300
Rafael Ferreira Gomes <rafael.gomes@pravaler.com.br>
2020-12-10 02:15:27 -0300

    Add Controller code

--- sales-api/app/Http/Controllers/SalesController.php
+++ sales-api/app/Http/Controllers/SalesController.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Services\SalesService;
+use Illuminate\Http\JsonResponse;
+
 class SalesController extends Controller
 {
+  /**
+   *
+   * @var SalesService
+   */
+  private $salesService;
+
   /**
    * Create a new controller instance.
    *
    * @return void
    */
-  public function __construct()
+  public function __construct(SalesService $salesService)
   {
-    //
+    $this->salesService = $salesService;
   }
 
-  public function uploadSales()
+  public function uploadSales(Request $request): JsonResponse
   {
-    //Code here.....
+    return $this->salesService->uploadSales($request);
   }
 }
